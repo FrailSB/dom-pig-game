3a76bdfda814b771b6c9fa92962abbee03ea7e4c
John Frail Bongat <jfraildev@gmail.com>
2020-02-08 22:26:28 +0800

    Roll dice button can now generate random numbers and show the corresponding dice number

--- js/script.js
+++ js/script.js
@@ -9,21 +9,37 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
-
+var scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.dice').style.display = 'none';
 
-// Rewrite html element
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = '/img/dice-' + dice + '.png';
+
+  document.querySelector('#current-' + activePlayer).textContent = dice;
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+});
+
+// Rewrite html element (Manipulate)
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML =
 //   '<em>' + dice + '</em>';
 
-// Read from html document
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-document.querySelector('.dice').style.display = 'none';
+// Read from html document (Access)
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
